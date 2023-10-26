efdd8a35723eec80b86a02e78b60dd6739c600db
tomkintzel <kintzel@ecommerce-werkstatt.de>
2023-10-26 23:12:54 +0200

    add can

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,3 +1,16 @@
+<script setup>
+const route = useRoute()
+let canPath = route.path.length > 1 ? route.path : ''
+console.log(route.name)
+useHead({
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://www.frohn-steuerberater.de' + canPath
+    }
+  ]
+})
+</script>
 <template>
   <main>
     <ContentDoc :class="$route.path.slice(1)" />
